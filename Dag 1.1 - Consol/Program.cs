d46fe0a4ff910ce4b478e03a1e960d9e2c15a644
donzazi <ellyas20@live.dk>
2024-02-19 15:35:26 +0100

    2/5 complete

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -4,6 +4,67 @@ Console.WriteLine("Hello, World!");
 
 Console.Write("Congratulations!");
 Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+Console.WriteLine("You wrote your first lines of code.");
+
+//Complete the challenge
+
+//first example
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
+Console.WriteLine(" ");
+
+// second example
+Console.WriteLine("This is the first line.");
+Console.Write("This ");
+Console.Write("is ");
+Console.Write("the ");
+Console.Write("second ");
+Console.WriteLine("line.");
+Console.WriteLine(" ");
+
+// Use character literals
+Console.WriteLine('b');
+Console.WriteLine(" ");
+
+// Use integer literals
+Console.WriteLine(123);
+Console.WriteLine(" ");
+
+// Use floating-point literals
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(" ");
+
+// Use Boolean literals
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine(" ");
+
+// Exercise - Working with variables
+// Create your first variable
+string firstName;
+firstName = "Don";
+Console.WriteLine(firstName);
+
+// Reassign the value of a variable
+firstName = "Pablo";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+// Initialize the variable
+string first_Name = "DonZazi";
+Console.WriteLine(first_Name);
+Console.WriteLine(" ");
+
+
+// Challenge: Display literal and variable values
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+Console.WriteLine("Hello, " + name + "! You have " + messages + " messages in your inbox. The temeperature is " + temperature + " celcius.");
 
 
