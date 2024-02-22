c3427a0783731cb1cd24c3751a99594c8b5331d1
donzazi <ellyas20@live.dk>
2024-02-22 12:45:18 +0100

    bullshit.

--- Dag 2.1 - ConsolApp/Program.cs
+++ Dag 2.1 - ConsolApp/Program.cs
@@ -42,10 +42,8 @@ int secondValue = 600;
 int largerValue = Math.Max(firstValue, secondValue);
 
 Console.WriteLine(largerValue);
-Console.WriteLine("\n");
-Console.WriteLine("\n");
 
-
+//=========================================================================================
 
 Random dice = new Random();
 
@@ -61,22 +59,115 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
     Console.WriteLine("You rolled doubles! +2 bonus to toal");
     total += 2;
 }
+
 if ((roll1 == roll2) && (roll2 == roll3))
 {
     Console.WriteLine("You rolled triples! +6 bonus to tal");
     total += 6;
 }
+
 if (total >= 15)
 {
     Console.WriteLine("You Win!");
 }
+
 if (total < 15)
 {
     Console.WriteLine("Sorry, you lose!");
 }
 
+Console.WriteLine("\n");
+
+
+//=========================================================================================
+
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+
+Console.WriteLine("\n");
+
+//=========================================================================================
+
+/* 
+string[] fraudulentOrderIDs = new string[3];
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789"; */
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign Value: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
 
 
 Console.WriteLine("\n");
+//=========================================================================================
+
+
+
+string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in the inventory.");
+
+
 Console.WriteLine("\n");
+//=========================================================================================
+
+
+string[] fraudIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235,", "179}" };
+
+foreach (string IDs in fraudIDs)
+{
+    if (IDs.StartsWith("B"))
+    {
+        Console.WriteLine(IDs);
+    }
+}
 
--- Dag 2.2 - Guided Project/Program.cs
+++ Dag 2.2 - Guided Project/Program.cs
@@ -2,7 +2,58 @@
 //Starter koden
 // initialize variables - graded assignments 
 int currentAssignments = 5;
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "logan" };
+int[] studentScores = new int[10];
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+
+    foreach (int score in studentScores)
+    {
+        // add the exam score to the sum
+        sumAssignmentScores += score;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
+
+
+
+
+
+/*
 int sophia1 = 90;
 int sophia2 = 86;
 int sophia3 = 87;
@@ -25,36 +76,13 @@ int logan1 = 90;
 int logan2 = 95;
 int logan3 = 87;
 int logan4 = 88;
-int logan5 = 96;
+int logan5 = 96; 
+*/
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
 
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
 
 
 
